41084ab101f5270bd445774d1bd87d5cdb6aceb9
Aguwungg <agunghendi.irawan@gmail.com>
2026-06-01 00:47:29 +0700

    Integrasi UI Frontend ke Backend Express

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path'); // TAMBAHAN: Modul bawaan Node.js untuk mengatur folder
 
-// MENGHUBUNGKAN FILE ROUTES
 const authRoutes = require('./routes/auth'); 
-const pesananRoutes = require('./routes/pesanan'); // Tambahan file rute pesanan
+const pesananRoutes = require('./routes/pesanan');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// MENDAFTARKAN JALUR API
-app.use('/api/auth', authRoutes); 
-app.use('/api/pesanan', pesananRoutes); // Mengaktifkan jalur API pesanan
+// TAMBAHAN: Memberi tahu Express lokasi folder UI kita
+app.use(express.static(path.join(__dirname, 'public')));
 
-// JALUR UTAMA (Test)
+app.use('/api/auth', authRoutes); 
+app.use('/api/pesanan', pesananRoutes);
+
+// UBAH JALUR UTAMA: Daripada mengirim teks, kita kirim file HTML
 app.get('/', (req, res) => {
-  res.send('Mantap! Server dan Database sudah siap menerima perintah!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 console.log('⏳ Sedang mencoba menghubungi MongoDB Atlas dengan IPv4...');
 
-// KONEKSI KE MONGODB
 mongoose.connect(process.env.MONGO_URI, {
   serverSelectionTimeoutMS: 5000,
   family: 4 
